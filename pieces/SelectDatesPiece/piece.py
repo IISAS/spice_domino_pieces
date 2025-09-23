908dc1f7b490ff99dd1f7d7ff326a0b5fc3e3b2c
Martin Šeleng <pakoselo@users.noreply.github.com>
2025-09-23 09:42:02 +0200

    Select dates piece

---
+++ pieces/SelectDatesPiece/piece.py
@@ -0,0 +1,29 @@
+from domino.base_piece import BasePiece
+from .models import InputModel, OutputModel
+import pandas as pd
+from pathlib import Path
+
+
+class SelectDatesPiece(BasePiece):
+
+    def piece_function(self, input_data: InputModel):
+
+        df_data = pd.read_csv(input_data.fve_input_file)
+
+        df_sel_data = (df_data['DateTime'] > input_data.date_start) & (df_data['DateTime'] <= input_data.date_end)
+
+        message = f"FVE data successfully filtered by dates"
+        file_path = str(Path(self.results_path) / "FVE_filtered.csv")
+        df_sel_data.to_csv(file_path, index=False)
+
+        # Set display result
+        self.display_result = {
+            "file_type": "csv",
+            "file_path": file_path
+        }
+
+        # Return output
+        return OutputModel(
+            message=message,
+            file_path=file_path
+        )
