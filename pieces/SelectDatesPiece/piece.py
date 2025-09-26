b6767c95536ee41ce3afcfac3f20458aa0b15108
Martin Šeleng <pakoselo@users.noreply.github.com>
2025-09-26 13:14:51 +0200

    check the datetime object

--- pieces/SelectDatesPiece/piece.py
+++ pieces/SelectDatesPiece/piece.py
@@ -2,6 +2,7 @@ from domino.base_piece import BasePiece
 from .models import InputModel, OutputModel
 import pandas as pd
 from pathlib import Path
+from datetime import datetime, timezone
 
 
 class SelectDatesPiece(BasePiece):
@@ -9,7 +10,7 @@ class SelectDatesPiece(BasePiece):
     def piece_function(self, input_data: InputModel):
 
         df_data = pd.read_csv(input_data.fve_input_file, parse_dates=['DateTime'])
-        self.logger.info(df_data.iloc[0]['DateTime'], input_data.date_start, input_data.date_end)
+        self.logger.info(df_data.iloc[0]['DateTime'], input_data.date_start, input_data.date_end, datetime(input_data.date_start, tzinfo=timezone.utc), datetime(input_data.date_end, tzinfo=timezone.utc))
         print(df_data.iloc[0]['DateTime'], input_data.date_start, input_data.date_end)
 
         df_sel_data = (df_data['DateTime'] > input_data.date_start) & (df_data['DateTime'] <= input_data.date_end)
