5f8256be8a7714ed9fcdc27c891986a13521657d
Martin Šeleng <pakoselo@users.noreply.github.com>
2025-09-29 13:59:30 +0200

    update

--- pieces/SelectDatesPiece/piece.py
+++ pieces/SelectDatesPiece/piece.py
@@ -10,9 +10,11 @@ class SelectDatesPiece(BasePiece):
 
         df_data = pd.read_csv(input_data.fve_input_file, parse_dates=['DateTime'])
         self.logger.info("MY LOG", df_data.iloc[0]['DateTime'], input_data.date_start, input_data.date_end)
-        print("MY LOG", df_data.iloc[0]['DateTime'], input_data.date_start, input_data.date_end)
+        print("MY LOG", df_data.iloc[0]['DateTime'], pd.to_datetime(input_data.date_start), pd.to_datetime(input_data.date_end))
 
-        df_sel_data = (df_data['DateTime'] > pd.to_datetime(input_data.date_start)) & (df_data['DateTime'] <= pd.to_datetime(input_data.date_end))
+        mask = (df_data['DateTime'] >= pd.to_datetime(input_data.date_start)) & (df_data['DateTime'] <= pd.to_datetime(input_data.date_end))
+
+        df_sel_data = df_data.loc[mask]
 
         message = f"Data successfully filtered by start and end dates"
         file_path = str(Path(self.results_path) / "data_filtered.csv")
