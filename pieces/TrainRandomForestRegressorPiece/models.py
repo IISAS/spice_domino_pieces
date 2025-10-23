ba98bea96772da98d021d8db0d1bf34ec17e66c0
stevo <stefan.dlugolinsky@savba.sk>
2025-10-23 12:54:37 +0200

    description fix

--- pieces/TrainRandomForestRegressorPiece/models.py
+++ pieces/TrainRandomForestRegressorPiece/models.py
@@ -17,7 +17,7 @@ class InputModel(BaseModel):
 
     train_data_path: str = Field(
         title="Train Data Path",
-        description="The path to the train data to train the data.",
+        description="The path to the train data to train the model.",
         # json_schema_extra={"from_upstream": "always"}
     )
 
