fd9dfff8de46758e53bd8c5f78e47c270b4805df
stevo <stefan.dlugolinsky@savba.sk>
2025-10-27 10:55:17 +0100

    skip tests in github actions

--- pieces/KafkaConsumerPiece/test_kafka_consumer_piece.py
+++ pieces/KafkaConsumerPiece/test_kafka_consumer_piece.py
@@ -6,6 +6,7 @@ from typing import List
 from unittest.mock import patch
 
 from domino.testing import piece_dry_run
+from domino.testing.utils import skip_envs
 from mockafka import FakeConsumer, FakeProducer, FakeAdminClientImpl
 from mockafka.admin_client import NewTopic
 
@@ -42,45 +43,46 @@ def run_piece(
     )
 
 
-# def test_kafka_consumer_piece():
-#     piece_kwargs = {
-#         "topics": ['test-topic1', 'test-topic2'],
-#         "bootstrap_servers": 'spice-kafka-broker-1.stevo.fedcloud.eu:9093',
-#         "group_id": "test-group",
-#         "security_protocol": "SSL",
-#     }
-#
-#     num_partitions = 5
-#
-#     # Create topics
-#     admin = FakeAdminClientImpl()
-#     admin.create_topics([
-#         NewTopic(topic=topic, num_partitions=num_partitions) for topic in piece_kwargs['topics']
-#     ])
-#
-#     # Produce messages
-#     producer = FakeProducer()
-#     for i in range(0, 10):
-#         topic = piece_kwargs['topics'][randint(0, len(piece_kwargs['topics']) - 1)]
-#         key = f'test_key{randint(0, 3)}'
-#         value = f'test_value{i}'
-#         partition = randint(0, num_partitions - 1)
-#         timestamp = int(datetime.now().timestamp() * 1000)
-#         producer.produce(
-#             topic=topic,
-#             key=key,
-#             value=value,
-#             partition=partition,
-#             timestamp=timestamp
-#         )
-#         sleep(randint(1, 3))
-#
-#     # Subscribe consumer
-#     consumer = FakeConsumer()
-#     consumer.subscribe(topics=piece_kwargs['topics'])
-#
-#     with patch('confluent_kafka.Consumer', new=FakeConsumer):
-#         output = run_piece(
-#             **piece_kwargs
-#         )
-#         print(output)
+@skip_envs('github')
+def test_kafka_consumer_piece():
+    piece_kwargs = {
+        "topics": ['test-topic1', 'test-topic2'],
+        "bootstrap_servers": 'spice-kafka-broker-1.stevo.fedcloud.eu:9093',
+        "group_id": "test-group",
+        "security_protocol": "SSL",
+    }
+
+    num_partitions = 5
+
+    # Create topics
+    admin = FakeAdminClientImpl()
+    admin.create_topics([
+        NewTopic(topic=topic, num_partitions=num_partitions) for topic in piece_kwargs['topics']
+    ])
+
+    # Produce messages
+    producer = FakeProducer()
+    for i in range(0, 10):
+        topic = piece_kwargs['topics'][randint(0, len(piece_kwargs['topics']) - 1)]
+        key = f'test_key{randint(0, 3)}'
+        value = f'test_value{i}'
+        partition = randint(0, num_partitions - 1)
+        timestamp = int(datetime.now().timestamp() * 1000)
+        producer.produce(
+            topic=topic,
+            key=key,
+            value=value,
+            partition=partition,
+            timestamp=timestamp
+        )
+        sleep(randint(1, 3))
+
+    # Subscribe consumer
+    consumer = FakeConsumer()
+    consumer.subscribe(topics=piece_kwargs['topics'])
+
+    with patch('confluent_kafka.Consumer', new=FakeConsumer):
+        output = run_piece(
+            **piece_kwargs
+        )
+        print(output)
--- pieces/TimeSeriesClassificationTrainPiece/test_TimeSeriesClassificationTrain_piece.py
+++ pieces/TimeSeriesClassificationTrainPiece/test_TimeSeriesClassificationTrain_piece.py
@@ -2,6 +2,7 @@ from typing import List
 
 import numpy as np
 from domino.testing import piece_dry_run
+from domino.testing.utils import skip_envs
 from tensorflow import keras
 from tensorflow.keras.layers import Conv1D
 
@@ -27,25 +28,26 @@ def run_piece(
     )
 
 
-# def test_TimeSeriesClassificationTrainPiece():
-#     piece_kwargs = {
-#         'train_data_path': 'https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/FordA_TRAIN.tsv',
-#         'num_layers': 3,
-#         'filters_per_layer': [64] * 3,
-#         'kernel_sizes': [3] * 3,
-#         'batch_size': 32,
-#         'epochs': 5
-#     }
-#     output = run_piece(
-#         **piece_kwargs
-#     )
-#     m = keras.models.load_model(output['best_model_file_path'])
-#     conv1D_layers = [layer for layer in m.layers if isinstance(layer, Conv1D)]
-#     assert len(conv1D_layers) == piece_kwargs['num_layers']
-#     for layer, filters, kernel_size in zip(
-#         conv1D_layers,
-#         piece_kwargs['filters_per_layer'],
-#         piece_kwargs['kernel_sizes']
-#     ):
-#         assert layer.filters == filters
-#         assert layer.kernel_size == np.reshape(kernel_size, (1))
+@skip_envs('github')
+def test_TimeSeriesClassificationTrainPiece():
+    piece_kwargs = {
+        'train_data_path': 'https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/FordA_TRAIN.tsv',
+        'num_layers': 3,
+        'filters_per_layer': [64] * 3,
+        'kernel_sizes': [3] * 3,
+        'batch_size': 32,
+        'epochs': 5
+    }
+    output = run_piece(
+        **piece_kwargs
+    )
+    m = keras.models.load_model(output['best_model_file_path'])
+    conv1D_layers = [layer for layer in m.layers if isinstance(layer, Conv1D)]
+    assert len(conv1D_layers) == piece_kwargs['num_layers']
+    for layer, filters, kernel_size in zip(
+        conv1D_layers,
+        piece_kwargs['filters_per_layer'],
+        piece_kwargs['kernel_sizes']
+    ):
+        assert layer.filters == filters
+        assert layer.kernel_size == np.reshape(kernel_size, (1))
