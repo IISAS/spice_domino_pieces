6274d3ad2b4bf639927bdabb73ff9c7e07b37958
stevo <stefan.dlugolinsky@savba.sk>
2025-10-29 14:41:03 +0100

    model fix

--- pieces/TimeSeriesClassificationTrainPiece/models.py
+++ pieces/TimeSeriesClassificationTrainPiece/models.py
@@ -15,7 +15,7 @@ class InputModel(BaseModel):
         # json_schema_extra={"from_upstream": "always"}
     )
 
-    validation_data_path: str = Field(
+    validation_data_path: str | None = Field(
         title="validation_data_path",
         description="Path to the validation data.",
         default=None
@@ -30,53 +30,55 @@ class InputModel(BaseModel):
     skiprows_train: int = Field(
         title="skiprows_train",
         description="The number of rows to skip in the train data",
+        default=0
     )
 
     skiprows_val: int = Field(
         title="skiprows_val",
         description="The number of rows to skip in the validation data (if specified)",
+        default=0
     )
 
     num_layers: int = Field(
         title="num_layers",
-        default=3,
-        description="Number of convolutional layers."
+        description="Number of convolutional layers.",
+        default=3
     )
 
     filters_per_layer: List[int] = Field(
         title="filters_per_layer",
-        default=[64, 64, 64],
-        description="Number of filters for each convolutional layer."
+        description="Number of filters for each convolutional layer.",
+        default=[64, 64, 64]
     )
 
     kernel_sizes: List[int] = Field(
         title="kernel_sizes",
-        default=[3, 3, 3],
-        description="Kernel size for each convolutional layer."
+        description="Kernel size for each convolutional layer.",
+        default=[3, 3, 3]
     )
 
     batch_size: int = Field(
         title="batch_size",
-        default=32,
-        description="Batch size."
+        description="Batch size.",
+        default=32
     )
 
     epochs: int = Field(
         title="epochs",
-        default=500,
-        description="Number of epochs."
+        description="Number of epochs.",
+        default=500
     )
 
     shuffle: bool = Field(
         title="shuffle",
-        default=False,
-        description="Shuffle training data before each epoch."
+        description="Shuffle training data before each epoch.",
+        default=False
     )
 
     validation_split: float = Field(
         title="validation_split",
-        default=0.2,
-        description="Validation split if no validation data provided."
+        description="Validation split if no validation data provided.",
+        default=0.2
     )
 
 
